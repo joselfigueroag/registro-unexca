455c84dd493dad171d6b1dd3b5ae5b22b0caaf10
jose <joselfigueroag@gmail.com>
2022-10-05 09:26:12 -0400

    *home customized *added community and university images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,79 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-10">
+                <div class="card">
+                    <div class="card-header bg-primary">{{ __('Tablero') }}</div>
+                    <div class="card-body d-flex justify-content-between">
+                        <div class="col-5">
+                            @hasrole('admin')
+                                <div class="dropdown mb-3">
+                                    <button class="col-12 btn btn-primary dropdown-toggle" type="button" id="dropdownEmployee"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        Empleado
+                                    </button>
+                                    <div class="dropdown-menu p-0 col-12" aria-labelledby="dropdownEmployee">
+                                        <a href="{{ route('register') }}" class="btn btn-primary btn-lg col-12 rounded-0"
+                                            role="button" aria-pressed="true">Registrar empleado</a>
+                                        <hr class="dropdown-divider m-0 p-0 col-12">
+                                        <a href="#" class="btn btn-primary btn-lg col-12 rounded-0" role="button"
+                                            aria-pressed="true">Consultar empleado</a>
+                                        <hr class="dropdown-divider m-0 p-0 col-12">
+                                        <a href="#" class="btn btn-primary btn-lg col-12 rounded-0" role="button"
+                                            aria-pressed="true">Editar empleado</a>
+                                        <hr class="dropdown-divider m-0 p-0 col-12">
+                                        <a href="#" class="btn btn-primary btn-lg col-12 rounded-0" role="button"
+                                            aria-pressed="true">Eliminar empleado</a>
+                                    </div>
+                                </div>
+                            @endhasrole
+                            <div class="dropdown mb-3">
+                                <button class="col-12 btn btn-primary dropdown-toggle" type="button" id="dropdownUser"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Usuario
+                                </button>
+                                <div class="dropdown-menu p-0 col-12" aria-labelledby="dropdownUser">
+                                    <a href="{{ route('register_patient') }}"
+                                        class="btn btn-primary btn-lg col-12 rounded-0" role="button"
+                                        aria-pressed="true">Registrar usuario</a>
+                                    <hr class="dropdown-divider m-0 p-0 col-12">
+                                    <a href="{{ route('list_patients') }}" class="btn btn-primary btn-lg col-12 rounded-0" role="button"
+                                        aria-pressed="true">Listar usuarios</a>
+                                    <hr class="dropdown-divider m-0 p-0 col-12">
+                                    <a href="#" class="btn btn-primary btn-lg col-12 rounded-0" role="button"
+                                        aria-pressed="true">Editar usuario</a>
+                                    <hr class="dropdown-divider m-0 p-0 col-12">
+                                    <a href="#" class="btn btn-primary btn-lg col-12 rounded-0" role="button"
+                                        aria-pressed="true">Eliminar usuario</a>
+                                </div>
+                            </div>
+                            {{-- <div class="dropdown mb-3">
+                                <button class="col-12 btn btn-primary dropdown-toggle" type="button" id="dropdownEmployee"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Empleado
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownEmployee">
+                                    @hasrole('admin')
+                                        <a href="{{ route('register') }}" class="btn btn-primary btn-lg col-12">Registrar
+                                            empleado</a>
+                                    @endhasrole
+                                </div>
+                            </div> --}}
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                        <div class="col-6 text-center">
+                            <div>
+                                <img src="/images/logo_etalrs.jpg" alt="logo_etalrs">
+                            </div>
+                            <div>
+                                <h4>Escuela Tecnica Asistencial Luis Rodriguez Sanchez</h4>
+                                <h5>Laboratio Clinico</h5>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
